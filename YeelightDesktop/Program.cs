88fab5f226f62fbbf8f783bf1b24bf0d4ef940e1
mk <mefisto506@gmail.com>
2018-06-03 13:10:40 +0200

    first test ant it seems to work correctly

---
+++ YeelightDesktop/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using YeelightAPI;
+
+namespace YeelightDesktop
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+            Console.WriteLine("start");
+            test();
+            Console.WriteLine("end");
+
+        }
+
+        private static void test()
+        {
+            Console.WriteLine("ins");
+
+            Device device = new Device("192.168.0.105");
+            device.Connect();
+            device.SetPower(false);
+            Console.WriteLine(device);
+        }
+    }
+}
